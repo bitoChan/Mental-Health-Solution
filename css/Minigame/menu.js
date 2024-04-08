f43ef82fd1c8fa0eaa1273a9e6a71112aa84e959
BitoChan <122335942+bitoChan@users.noreply.github.com>
2024-04-08 11:55:09 +0800

    update

---
+++ css/Minigame/menu.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var startButton = document.querySelector('.menu-startbutton'); // 獲取開始按鈕
+    startButton.addEventListener('click', function() {
+      window.location.href = 'Minigame-introduction.html'; // 點擊時跳轉
+    });
+  });
+  
